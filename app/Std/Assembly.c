99eb742d7019dc4dda4c07bf4560f5f87399b253
Steve <jackblack@poppy>
2025-10-08 16:38:29 -0600

    remove extraneous crap

--- app/Std/Assembly.c
+++ app/Std/Assembly.c
@@ -1,25 +1,3 @@
 #include <stdio.h>
 
-void print(const char *msg) {
-  puts(msg);
-}
-
-void printInt(const int i) {
-  printf("%i", i);
-}
-
-int _43(const int a, const int b) {
-  return a + b;
-}
-
-int _45(const int a, const int b) {
-  return a - b;
-}
-
-int _42(const int a, const int b) {
-  return a * b;
-}
-
-int _47(const int a, const int b) {
-  return a / b;
-}
+void print(const char *msg) { puts(msg); }
